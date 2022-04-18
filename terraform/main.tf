b0501d03794ea53c091c2a25954acee3a6da6aef
wpniederer <wpniederer@gmail.com>
2022-04-17 20:21:01 -0700

    Remove vars for scalr

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,26 @@
-variable "scalr_org_id" {
-  type = string
-}
-
-variable "scalr_hostname" {
-  type = string
-}
-
-variable "scalr_workspace_name" {
+variable "aws_region" {
   type    = string
-  default = "Prod"
+  default = "us-west-2"
 }
 
 terraform {
   backend "remote" {
-    hostname     = var.scalr_hostname
-    organization = var.scalr_org_id
+    hostname     = "wpniederer.scalr.io"
+    organization = "env-u0grbkhnofp78lo"
 
     workspaces {
-      name = var.scalr_workspace_name
+      name = "CLI"
     }
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
 }
