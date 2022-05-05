22b6d5e7ca734b628b57248890ad6a159e535b20
wpniederer <wpniederer@gmail.com>
2022-05-05 11:35:18 -0700

    Use new template

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,35 +1,32 @@
-import Container from './container'
+import Link from './Link'
+import siteMetadata from '@/data/siteMetadata'
+import SocialIcon from '@/components/social-icons'
 
 export default function Footer() {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
-      <Container>
-        <div className="py-8 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-             Contact me
-          </h3>
-          <div className="flex flex-col lg:flex-col justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href={`https://github.com/wpniederer`}
-              className="mx-3 font-bold hover:underline"
-            >
-              Walt@GitHub
-            </a>
-            <a
-              href={`mailto:wpniederer@gmail.com`}
-              className="mx-3 font-bold hover:underline"
-            >
-              Walt@Email
-            </a>
-            <a
-              href={`https://www.linkedin.com/in/walter-niederer/`}
-              className="mx-3 font-bold hover:underline"
-            >
-              Walt@LinkedIn
-            </a>
-          </div>
+    <footer>
+      <div className="mt-16 flex flex-col items-center">
+        <div className="mb-3 flex space-x-4">
+          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
+          <SocialIcon kind="github" href={siteMetadata.github} size="6" />
+          <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" />
+          <SocialIcon kind="youtube" href={siteMetadata.youtube} size="6" />
+          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
+          <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
         </div>
-      </Container>
+        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+          <div>{siteMetadata.author}</div>
+          <div>{` • `}</div>
+          <div>{`© ${new Date().getFullYear()}`}</div>
+          <div>{` • `}</div>
+          <Link href="/">{siteMetadata.title}</Link>
+        </div>
+        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
+            Tailwind Nextjs Theme
+          </Link>
+        </div>
+      </div>
     </footer>
   )
 }
