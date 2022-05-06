b67952505daf0f02677b9d214d3ca06faafe220b
wpniederer <wpniederer@gmail.com>
2022-05-06 13:26:12 -0700

    Add spotify now playing component

---
+++ components/Equalizer.js
@@ -0,0 +1,9 @@
+export default function Equalizer() {
+  return (
+    <div className="gap-0.75 flex h-3 w-4 items-center">
+      <span className="w-0.75 motion-safe:animate-shrink h-3 rounded-sm bg-spotify-green" />
+      <span className="w-0.75 motion-safe:animate-expand h-1.5 rounded-sm bg-spotify-green" />
+      <span className="w-0.75 motion-safe:animate-shrink h-3 rounded-sm bg-spotify-green" />
+    </div>
+  )
+}
