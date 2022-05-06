ae9fd33aa7f1230345af013802f02be775d2995c
wpniederer <wpniederer@gmail.com>
2022-05-06 15:37:45 -0700

    Adjust layout for now playing component

--- components/Equalizer.js
+++ components/Equalizer.js
@@ -1,6 +1,6 @@
 export default function Equalizer() {
   return (
-    <div className="gap-0.75 flex h-3 w-4 items-center">
+    <div className="flex h-1 w-4 items-center gap-0.5">
       <span className="w-0.75 motion-safe:animate-shrink h-3 rounded-sm bg-spotify-green" />
       <span className="w-0.75 motion-safe:animate-expand h-1.5 rounded-sm bg-spotify-green" />
       <span className="w-0.75 motion-safe:animate-shrink h-3 rounded-sm bg-spotify-green" />
