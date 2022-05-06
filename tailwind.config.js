c0a621982981f90d70557c3de7461f5288ff1616
wpniederer <wpniederer@gmail.com>
2022-05-05 22:00:20 -0700

    Update styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,8 +28,8 @@ module.exports = {
         sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        primary: colors.teal,
-        gray: colors.neutral,
+        primary: colors.red,
+        gray: colors.slate,
       },
       typography: (theme) => ({
         DEFAULT: {
@@ -63,7 +63,7 @@ module.exports = {
               backgroundColor: theme('colors.gray.800'),
             },
             code: {
-              color: theme('colors.pink.500'),
+              color: theme('colors.amber.500'),
               backgroundColor: theme('colors.gray.100'),
               paddingLeft: '4px',
               paddingRight: '4px',
