e35953f2c41056ddc3e7ffe7a143e0a434cab92c
wpniederer <wpniederer@gmail.com>
2022-05-26 20:25:14 -0700

    Update socialBanner

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/wpniederer/PersonalWebsite',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  // socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/site-card.png',
   email: 'wpniederer@gmail.com',
   github: 'https://github.com/wpniederer',
   // twitter: 'https://twitter.com/Twitter',
